5bca4eeff20f88cbdf176cc5938354bd964d776c
anandrajeshmishra <anand98425@gmail.com>
2025-11-22 16:06:16 +0530

    work

--- Day2.py
+++ Day2.py
@@ -5,3 +5,39 @@
 # print(type(age))
 # print(type(price))
 # print(type(False))
+# a = 50 
+# b = 30
+# print(not False)
+# print(not (a>b))
+
+# val1 = False
+# val2 = False
+# print("And operator :", val1 and val2)
+# print("Or operator: ", a == b or a > b)
+
+
+# Type converssion
+
+
+# a = 55
+# b = 24.5
+# sum = a + b
+# print(sum)
+
+# Type casting 
+
+# a = "55"
+# b = 24
+# a = int(a)
+# print(a+b)
+
+# name = input("Enter your name: ")
+# print("Welcome",name)
+
+# age = input("Enter your age: ")#there for we need to do the type casting to convert this string in to the integer
+# print(age)
+# print(type(age))
+
+age = int(input("Enter your age: "))
+print(age)
+print(type(age))#He He you did this
